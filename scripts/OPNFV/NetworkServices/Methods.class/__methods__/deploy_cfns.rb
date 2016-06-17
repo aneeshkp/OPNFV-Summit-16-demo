6c705bef8cd4fd4912df782ede67a1c1b4876fa4
Aneesh Puttur <aneeshputtur@gmail.com>
2016-06-17 13:27:57 -0400

    Fixed typo and error handling

--- scripts/OPNFV/NetworkServices/Methods.class/__methods__/deploy_cfns.rb
+++ scripts/OPNFV/NetworkServices/Methods.class/__methods__/deploy_cfns.rb
@@ -1,181 +1,129 @@
-def template_interface(name, subnet_id, security_group)
-  eip = "#{name}Address"
-  eip_association = "Associate#{name}"
-  network_interface = "#{name}"
-  {
-    eip => {
-      "Type"=>"AWS::EC2::EIP",
-      "Properties"=>{"Domain"=>"vpc"}},
-    eip_association => {
-      "Type"=>"AWS::EC2::EIPAssociation",
-      "Properties"=>
-      {"AllocationId"=>{"Fn::GetAtt"=>[eip, "AllocationId"]},
-        "NetworkInterfaceId"=>{"Ref"=>network_interface}}},
-    network_interface => {
-      "Type"=>"AWS::EC2::NetworkInterface",
-      "Properties" => {
-        "SubnetId"=> subnet_id,
-        "Description"=>"",
-        "GroupSet"=>[{"Ref"=>security_group}],
-        "SourceDestCheck"=>"true",
-        "Tags"=>[{"Key"=>"Network", "Value"=>"Control"}]}},
-  }
-end
+def get_template_contents(network_service)
+  template_contents = {}
+  network_service.direct_service_children.detect { |x| x.name == 'VNF templates' }.direct_service_children.each do |vnfd_service|
+    orchestration_template_service = vnfd_service.direct_service_children.first
+    raise "Undefined VNF template for #{vnfd_service.name}" if orchestration_template_service.nil?
 
-def security_group(security_group, vpc_id)
-  {
-    security_group =>
-    {"Type"=>"AWS::EC2::SecurityGroup",
-      "Properties"=>
-      {"VpcId"=> vpc_id,
-        "GroupDescription"=>"Enable SSH access via port 22",
-        "SecurityGroupIngress"=>
-        [{"IpProtocol"=>"tcp",
-            "FromPort"=>"22",
-            "ToPort"=>"22",
-            "CidrIp"=>"0.0.0.0/0"}]}},
-  }
-end
-
-def security_group_1(security_group, vpc_id)
-  {
-    security_group =>
-    {"Type"=>"AWS::EC2::SecurityGroup",
-      "Properties"=>
-      {"VpcId" => vpc_id,
-        "GroupDescription"=>"Enable HTTP access via user defined port",
-        "SecurityGroupIngress"=>
-        [{"IpProtocol"=>"tcp",
-            "FromPort"=>80,
-            "ToPort"=>80,
-            "CidrIp"=>"0.0.0.0/0"}]}},
-  }
-end
-
-def instance(name, network_interfaces, keyname, image_id,availability_zone,instance_type)
-  nics = network_interfaces.each_with_index.map do |x, i|
-    {"NetworkInterfaceId" => {"Ref" => x}, "DeviceIndex" => i.to_s}
-  end
-
-  {
-    name =>
-    {"Type"=>"AWS::EC2::Instance",
-      "Properties"=> {
-        "InstanceType" => instance_type,
-        "ImageId"=> image_id,
-        "KeyName"=> keyname,
-        "AvailabilityZone"=>availability_zone,
-        "NetworkInterfaces"=> nics,
-        "Tags"=>[{"Key"=>"Role", "Value"=>"Test Instance"}],
-        "UserData"=>
-        {"Fn::Base64"=>
-          {"Fn::Join"=>
-            ["",
-              ["#!/bin/bash -ex",
-                "\n",
-                "\n",
-                "yum install ec2-net-utils -y",
-                "\n",
-                "ec2ifup eth1",
-                "\n",
-                "service httpd start"]]}}}}
-  }
-end
-
-def base_template(name)
-  {
-    "Description" => "#{name} CFN template",
-    "Resources"   => {}
-  }
-end
-
-def create_template(name,nsd_properties,nsd_requirements)
-  vpc_id = nsd_properties['vpc_id'] #'vpc-303bc157'
-  image_id = nsd_properties['image_id']
-  subnet=nil
-  security_group=nil
-  availability_zone=nsd_properties['availabilityzone']
-  key_name=nsd_properties['key_name']
-  instance_type=nsd_properties['image_type']
-  template_content = base_template(name)
-  nic = []
-
-  network=$evm.vmdb('ManageIQ_Providers_Amazon_NetworkManager_CloudNetwork').find_by_name(vpc_id.strip)
-  image=$evm.vmdb('ManageIQ_Providers_Amazon_CloudManager_Template').find_by_name(image_id.strip)
+    template_content = JSON.parse(orchestration_template_service.custom_get('vnfd_template'))
+    $evm.log("info", "Template VNFd #{template_content}")
+    
+    template_contents[vnfd_service.name] = template_content
+  end 
   
-  if network==nil
-    raise "vpc_id #{vpc_id} for amazon is not valid."
-  elsif image==nil
-    raise "Image id #{image_id} for amazon is not valid."
-  elsif availability_zone==nil
-    raise "availabilityzone is nil"
-  end
-  # set default instance_type
-  if  instance_type==nil
-    instance_type="t2.medium"
-  end
+  template_contents
+end  
 
-  nsd_requirements.each do|requirement|
-    requirement.each do|key,value|
-      $evm.log(:info ,"finding #{key}  and #{value}")
-      if value!=nil
-        #check if manageIQ database has the interface and get it's id
-        $evm.log(:info ,"finding #{value} subnet in amazon")
-        subnet=$evm.vmdb('ManageIQ_Providers_Amazon_NetworkManager_CloudSubnet').find_by_name(value.strip)
-        if subnet==nil
-          $evm.log(:error,"#{value} subnet not found in amazon")
-          exit MIQ_ERROR
+def get_template(orchestration_manager, network_service, parent_service, vnf_service)
+  templates = {}
+  # Load our general VNFDs per type
+  template_contents = get_template_contents(network_service)
+  
+  # Upload specific VNFD per VNF, defined in NSD, into Tacker
+  unused_cps    = []
+  virtual_links = {}
+    
+  template_content = JSON.parse(template_contents[vnf_service.custom_get('type')].to_json.dup)
+  $evm.log("info", "Template VNFD: #{template_content}")
+  $evm.log("info", "Template Type: #{vnf_service.custom_get('type')}")
+    
+  # substitution_mappings_requirements format {virtualLink1:    [CP1, virtualLink]}
+  substitution_mappings_requirements = template_content['topology_template']['substitution_mappings']['requirements']
+  $evm.log("info", "Substitution mappings requirements Type: #{substitution_mappings_requirements}")  
+  
+  vnf_template_name = "#{parent_service.name} #{vnf_service.name} #{vnf_service.custom_get('type')}"
+  $evm.log("info", "VNF template name: #{vnf_template_name}")
+
+  vnf_service_properties = JSON.parse(vnf_service.custom_get('properties'))
+  $evm.log("info", "VNF service properties: #{vnf_service_properties}")
+
+  template_content['topology_template']['node_templates'].each do |name, value|
+    $evm.log(:info, "Node template: #{name}, Value: #{value}")
+    if name =~ /VDU\d+/        
+      value['properties'].each do |property_name, property_value|
+        next if property_value =~ /\{.*?get_input\d/
+        next if ['mgmt_driver'].include?(property_name)
+        next if property_value.is_a?(Hash)
+          
+        if (new_value = vnf_service_properties[property_name])
+          $evm.log(:info, "Replacing: #{property_name}, with value #{new_value}")
+          value['properties'][property_name] = new_value
         end
-        nic<< value.gsub("\s", "").gsub("_", "")
-        security_group_name="DefaultSecurity"+value.gsub("\s", "").gsub("_", "")
-        template_content["Resources"].merge!(security_group(security_group_name, network.ems_ref))
-        template_content["Resources"].merge!(template_interface(value.gsub("\s", "").gsub("_", ""), subnet.ems_ref, security_group_name))
-      end
+      end  
+    elsif name =~ /CP\d+/
+      # vnf_service_requiremements format: [virtualLink1: VL1])
+      vnf_service_requirements = JSON.parse(vnf_service.custom_get('requirements'))
+      $evm.log("info", "VNF service requirements : #{vnf_service_requirements}")
+      
+      substitution_mapping = substitution_mappings_requirements.detect { |k, v| v.first == name }
+      link_name = substitution_mapping.first
+      link_type = substitution_mapping.second.second
+      network_name = vnf_service_requirements.detect { |x| x.keys.first == link_name }.values.first
+       
+      $evm.log("info", "VNF service procesing requirement: #{link_type}: #{link_name} with value: #{network_name}")
+
+      if network_name.blank?
+        # Mark CP as unused and delete it later from VNFD
+        $evm.log("info", "CP: #{name}, is unused and will be removed from the VNFD")
+        unused_cps << name
+      else
+        $evm.log("info", "Requirement: #{link_type} {node: #{link_name}}, is being added to #{name}")
+        # Add link under CP
+        # requirements:
+        #   - virtualLink: 
+        #       node: virtualLinkMgmt
+        value['requirements'] << { 
+          link_type => {
+            'node' =>link_name}}
+        
+        $evm.log("info", "#{link_type}: #{link_name} with network name #{network_name}, is being added to VNFD node_templates section")
+        # And store Virtual Link that will be added later
+        virtual_links[link_name] = {
+          'type'       => 'tosca.nodes.nfv.VL',
+          'properties' => {
+            'network_name' => "#{parent_service.name}_#{$evm.root['service_template_provision_task_id']}_#{network_name}",
+            'vendor'       => 'Tacker'}}
+      end  
     end
-  end
-  
-  $evm.log(:info ,"nic array==> #{nic}")
-  template_content["Resources"].merge!(instance("Ec2Instance", nic, key_name, image.ems_ref,availability_zone,instance_type))
-  $evm.vmdb('orchestration_template_cfn').create(
-  :name      => name,
-  :orderable => true,
-  :content   => JSON.pretty_generate(template_content))
+  end  
+    
+  template_content['topology_template']['node_templates'].merge!(virtual_links)
+  unused_cps.each { |x| template_content['topology_template']['node_templates'].delete(x) }
+      
+  $evm.vmdb('orchestration_template_vnfd').create(
+    :name      => vnf_template_name, 
+    :orderable => true, 
+    :ems_id    => orchestration_manager.id, 
+    :content   => YAML.dump(template_content))
 end
 
-def deploy_amazon_stack(orchestration_manager, parent_service, vnf_service)
-  nsd_properties = JSON.parse(vnf_service.custom_get('properties'))
-  nsd_requirements = JSON.parse(vnf_service.custom_get('requirements'))
-  nsd_capabilities = JSON.parse(vnf_service.custom_get('capabilities'))
+def deploy_vnfs(network_service, parent_service)
+  network_service.direct_service_children.detect { |x| x.name == 'VNFs' }.direct_service_children.each do |vnf_service| 
+    # TODO if Openstack stack or amazon VM
+    properties = JSON.parse(vnf_service.custom_get('properties'))
+    orchestration_manager = $evm.vmdb('ManageIQ_Providers_Openstack_CloudManager').find_by_name(properties['vim_id'])
+    next unless orchestration_manager
+    
+    deploy_vnf_stack(orchestration_manager, network_service, parent_service, vnf_service)
+  end  
+end
 
-  $evm.log("info", "Listing nsd_properties #{nsd_properties}")
-  $evm.log("info", "Listing nsd_requirements #{nsd_requirements}")
-  $evm.log("info", "Listing nsd_capabilities #{nsd_capabilities}")
+def deploy_vnf_stack(orchestration_manager, network_service, parent_service, vnf_service)
+  template = get_template(orchestration_manager, network_service, parent_service, vnf_service)
+  
+  # TODO should we filter passed params based on inputs in VNFD?
+  params = JSON.parse(vnf_service.custom_get('properties')).to_json
 
-  name = "#{parent_service.name} #{vnf_service.name}"
-  template = create_template(name,nsd_properties,nsd_requirements)
-
-  $evm.log("info", "Deploying CFN template #{name}")
   orchestration_service = $evm.vmdb('ServiceOrchestration').create(
-  :name => "#{parent_service.name} #{vnf_service.name}")
+    :name => "#{parent_service.name} #{vnf_service.name}")
 
-  orchestration_service.stack_name             = name.gsub("\s", "-").gsub("_", "-")
+  orchestration_service.stack_name             = "#{parent_service.name} #{vnf_service.name}"
   orchestration_service.orchestration_template = template
   orchestration_service.orchestration_manager  = orchestration_manager
-  orchestration_service.stack_options          = {}
+  orchestration_service.stack_options          = {:attributes => {:param_values => params}}
   orchestration_service.display                = true
-  orchestration_service.parent_service         = parent_servicename
-  orchestration_service.deploy_orchestration_stack
-end
-
-def deploy_cfns(network_service, parent_service)
-  network_service.direct_service_children.detect { |x| x.name == 'VNFs' }.direct_service_children.each do |vnf_service|
-    properties = JSON.parse(vnf_service.custom_get('properties'))
-    orchestration_manager = $evm.vmdb('ManageIQ_Providers_Amazon_CloudManager').find_by_name(properties['vim_id'])
-    next unless orchestration_manager
-
-    deploy_amazon_stack(orchestration_manager, parent_service, vnf_service)
-  end
-end
+  orchestration_service.parent_service         = parent_service
+  orchestration_service.deploy_orchestration_stack 
+end  
 
 begin
   nsd = $evm.get_state_var(:nsd)
@@ -183,15 +131,15 @@ begin
   $evm.log("info", "Listing Root Object Attributes:")
   $evm.root.attributes.sort.each { |k, v| $evm.log("info", "\t#{k}: #{v}") }
   $evm.log("info", "===========================================")
-
+  
   parent_service = $evm.root['service_template_provision_task'].destination
   parent_service.name = $evm.root.attributes['dialog_service_name']
-
+  
   network_service = $evm.vmdb('service', $evm.root.attributes['dialog_network_service'])
-
-  deploy_cfns(network_service, parent_service)
+  
+  deploy_vnfs(network_service, parent_service)
 rescue => err
-  $evm.log(:error, "[#{err}]\n#{err.backtrace.join("\n")}")
+  $evm.log(:error, "[#{err}]\n#{err.backtrace.join("\n")}") 
   $evm.root['ae_result'] = 'error'
   $evm.root['ae_reason'] = "Error: #{err.message}"
   exit MIQ_ERROR
